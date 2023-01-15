783e381784cd04ff4230a4be4f110d85ff30c7b7
rk121 <roshankhadka613@gmail.com>
2023-01-15 18:09:58 +1100

    commit tree 4c8272e8f3e418752da82528d823f7a6ef9c5ff0

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    if params[:name]
+      student = Student.where("first_name LIKE ?", "%" + params[:name] + "%" ).or(Student.where("last_name LIKE ?", "%" + params[:name] + "%" ))
+      render json: student
+    else 
+      students = Student.all
+      render json: students
+    end
   end
 
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
 end
